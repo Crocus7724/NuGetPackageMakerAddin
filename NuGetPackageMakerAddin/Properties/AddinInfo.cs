00f007b4245638582e8acefecffad56126f88309
Crocus7724 <account@crocus7724.net>
2016-10-07 11:52:54 +0900

    First commit

---
+++ NuGetPackageMakerAddin/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"NuGetPackageMakerAddin",
+	Namespace = "NuGetPackageMakerAddin",
+	Version = "1.0"
+)]
+
+[assembly: AddinName("NuGetPackageMakerAddin")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("NuGetPackageMakerAddin")]
+[assembly: AddinAuthor("Yamamoto")]
